fbdac45554a4e3e12d6ac251e211f3023cf9ff56
raulwad13 <rawad13@myyahoo.com>
2024-02-22 17:36:18 +0100

    easter egg created

---
+++ JS/easterEgg.js
@@ -0,0 +1,34 @@
+function easterEgg() {
+    update(locations[7]);
+  }
+  
+  function pickTwo() {
+    pick(2);
+  }
+  
+  function pickEight() {
+    pick(8);
+  }
+  
+  function pick(guess) {
+    const numbers = [];
+    while (numbers.length < 10) {
+      numbers.push(Math.floor(Math.random() * 11));
+    }
+    text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+    for (let i = 0; i < 10; i++) {
+      text.innerText += numbers[i] + "\n";
+    }
+    if (numbers.includes(guess)) {
+      text.innerText += "Right! You win 20 gold!";
+      gold += 20;
+      goldText.innerText = gold;
+    } else {
+      text.innerText += "Wrong! You lose 10 health!";
+      health -= 10;
+      healthText.innerText = health;
+      if (health <= 0) {
+        lose();
+      }
+    }
+  }
